49d1acc08b384c48e8f3bf005d795d550b73590d
kimminha <kimmjnha@gmail.com>
2023-10-18 17:06:20 +0900

    MainController 수정!

--- src/main/java/com/chickengame/mini/main/Controller/MainController.java
+++ src/main/java/com/chickengame/mini/main/Controller/MainController.java
@@ -3,7 +3,7 @@ package com.chickengame.mini.main.Controller;
 public class MainController {
 
     public static void main(String[] args) {
-        System.out.println("수정했습니다.");
+        System.out.println("수정!");
     }
 
 }
