6e6800e2e0a097ec4685b05c1f7b09410dca1b1a
Seopia <145535934+Seopia@users.noreply.github.com>
2023-10-20 13:10:02 +0900

    Add files via upload
    test

--- src/main/java/com/chickengame/mini/controller/GameManager.java
+++ src/main/java/com/chickengame/mini/controller/GameManager.java
@@ -1,5 +1,41 @@
 package com.chickengame.mini.controller;
 
-public class GameManager {
+import com.chickengame.mini.games.*;
+import com.chickengame.mini.model.dao.MemberDAO;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GameManager {
+    private List<Game> games;
+
+    Scanner sc = new Scanner(System.in);
+    public GameManager() {
+        games = new ArrayList<>();
+        games.add(new UpDownGame());
+        games.add(new SlotMachine777());
+        games.add(new Lotto());
+        // ...
+    }
+    public void gameStart(){
+
+        while (true) {
+            System.out.println(MemberDAO.getInstance().getMe().getName()+" 님 게임을 선택해주세요.");
+            System.out.println("1. Updown Game");
+            System.out.println("2. Slot Machine777");
+            System.out.println("3. Lotto 번호 추천");
+            System.out.println("9. 메인 메뉴로 돌아가기");
+            int num = sc.nextInt();
+            sc.nextLine();
+            if(num == 9){
+                return;
+            }
+            if (games.size() < num) {
+                System.out.println("다시 입력해주세요.");
+            } else {
+                games.get(num-1).gameStart();
+            }
+        }
+    }
 }
--- src/main/java/com/chickengame/mini/view/MainMenu.java
+++ src/main/java/com/chickengame/mini/view/MainMenu.java
@@ -1,8 +1,59 @@
 package com.chickengame.mini.view;
 
+import com.chickengame.mini.controller.GameManager;
+
+import java.util.Scanner;
+
 public class MainMenu {
-    public void mainmenu(){
-        System.out.println("==메인 메뉴==(깃 연습용 지섭이 테스트)");
-        return;
+    private GameManager gameManager = new GameManager();
+
+    private Scanner sc = new Scanner(System.in);
+
+    public void run() {
+        while (true) {
+            System.out.println("메인 메뉴 화면입니다.");
+            System.out.println("1. 전체 멤버 순위 출력");
+            System.out.println("2. 내 정보 출력");
+            System.out.println("3. 내 정보 수정");
+            System.out.println("4. 게임 시작");
+            System.out.println("5. 게임 종료");
+            System.out.print("메뉴: ");
+            int menu = sc.nextInt();
+            switch (menu) {
+                case 1:
+                    showRank();
+                    break;
+                case 2:
+                    showProfile();
+                    break;
+                case 3:
+                    updateProfile();
+                case 4:
+                    gameStart();
+                    break;
+                case 5:
+                    System.out.println("게임을 종료합니다.");
+                    return;
+                default:
+                    System.out.println("잘못 입력했습니다. 다시 입력하세요.");
+                    break;
+            }
+        }
+    }
+
+    private void showRank() {
+        MenuManager.getInstance().showRank();
+    }
+
+    private void showProfile() {
+        MenuManager.getInstance().showProfile();
+    }
+
+    private void updateProfile() {
+        MenuManager.getInstance().updateProfile();
+    }
+
+    private void gameStart() {
+        gameManager.gameStart();
     }
 }
